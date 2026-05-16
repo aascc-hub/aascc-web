90bb4f023f98c84234ca9534d4e18e31d562d3e6
mohd ashaar <Mohd ashaar.noreply.github.com>
2026-05-16 14:53:11 +0400

    EVENTS ID HAS BEEN CHANGED

--- src/data/events.ts
+++ src/data/events.ts
@@ -31,7 +31,7 @@ export const allEvents: EventType[] = [
   //   description: 'Fuziona is the premier inter-collegiate cultural and academic fest organized by Anjuman Arts, Science, Commerce College and PG Centre, Bhatkal. It serves as a dynamic platform for students to showcase their talents across 13+ diverse competitions, ranging from literary debates to cultural performances.'
   // },
   {
-    id: 2,
+    id: 1,
     title: 'Free Mega Health Camp and Cancer Awareness Programme',
     expectations: [
     'Specialist Consultations',
@@ -51,7 +51,7 @@ export const allEvents: EventType[] = [
     description: 'Anjuman Arts, Science and Commerce College, Bhatkal, is gearing up to host a Free Mega Health Camp and Cancer Awareness Program on 19th April 2026 at the Anjuman Degree College campus. The event is being organised in collaboration with Father Muller Medical College and Hospital, Mangalore. The camp will offer free specialist consultations across a wide range of medical departments, including General Medicine, Dermatology, General Surgery, ENT, Paediatrics, Ophthalmology, Obstetrics and Gynaecology, Orthopaedics, Speech and Hearing, and Surgical Oncology.',
   },
   {
-    id: 3,
+    id: 2,
     title: 'UKDFA Referee Examination',
     date: '2026-3-27',
     day: '27',
@@ -65,7 +65,7 @@ export const allEvents: EventType[] = [
     description: 'The Uttara Kannada District Football Association successfully hosted a three-day Referee Examination in Bhatkal from March 27th to 29th, 2026. The event was conducted under the supervision of the Karnataka State Football Association and its Referee Committee. A total of 28 aspirants participated in the program, representing various regions.',
   },
   {
-    id: 4,
+    id: 3,
     title: '1st Prize – Finance Event at Canvas 2025',
     date: '2024-11-29',
     day: '29',
@@ -79,7 +79,7 @@ export const allEvents: EventType[] = [
     description: 'Our students delivered an outstanding performance at Canvas 2025, the National-Level Management Fest hosted by Farook College, Kozhikode, Kerala, securing the 1st Prize in the Finance event.',
   },
   {
-    id: 5,
+    id: 4,
     title: 'Graduation Day',
     date: '2024-08-26',
     day: '26',
@@ -93,7 +93,7 @@ export const allEvents: EventType[] = [
     description: 'Celebrating the achievements of our graduating students. The event featured a formal ceremony where students received their diplomas, along with inspiring speeches from faculty members and guest speakers.'
   },
   {
-    id: 6,
+    id: 5,
     title: 'Personality And Skills Development Workshop',
     date: '2025-10-08',
     day: '08',
@@ -107,7 +107,7 @@ export const allEvents: EventType[] = [
     description: 'The Personality and Skills Development Workshop was conducted by AIMCA, Bhatkal on October 8, 2025. The event aimed to enhance students\' personal and professional skills through interactive sessions.'
   },
   {
-    id: 7,
+    id: 6,
     title: 'Independence Day Celebration',
     date: '2025-08-15',
     day: '15',
@@ -121,7 +121,7 @@ export const allEvents: EventType[] = [
     description: 'The Independence Day Celebration was a vibrant event held to commemorate the spirit of freedom and patriotism, featuring a flag hoisting ceremony and cultural performances.'
   },
   {
-    id: 8,
+    id: 7,
     title: 'Karnataka Rajyotsava Celebration',
     date: '2023-11-01',
     day: '01',
@@ -135,7 +135,7 @@ export const allEvents: EventType[] = [
     description: 'The Karnataka Rajyotsava Celebration was a vibrant event held to commemorate the formation of Karnataka state, showcasing the rich heritage and traditions of our state.'
   },
   {
-    id: 9,
+    id: 8,
     title: 'Road Safety Awareness Campaign',
     date: '2024-06-12',
     day: '12',
